ed4239d033384433237942cb9accfe7c9321ad7a
cratos300 <hernan-vilar@hotmail.com>
2018-07-22 23:04:00 -0300

    revisar
    revisar

---
+++ funciones.h
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "ArrayList.h"
+#ifndef _EMPLOYEE_H
+#define _EMPLOYEE_H
+typedef struct
+{
+    int idMensaje;
+    char mensaje[2000];
+    int popularidad;
+    int idUsuarioRela;
+}Mensajes;
+typedef struct
+{
+    int idUsuario;
+    char nick[5000];
+    int popularidad;
+}Usuarios;
+typedef struct
+{
+    int idMensaje;
+    char mensaje[50];
+    int popularidadMensaje;
+    int idUsuarioRela;
+    char nick[50];
+    int  popularidadUsuario;
+}Todo;
+
+int employee_compare(void* pEmployeeA,void* pEmployeeB);
+void employee_print(Mensajes* this);
+Mensajes* employee_new(void);
+int employee_compareApe(void* pEmployeeA,void* pEmployeeB);
+int employee_compareapellido(void* pEmployeeA,void* pEmployeeB);
+int menuModificar();
+void setIdMensaje(Mensajes *puntero,int idMensaje);
+void setmsg(Mensajes* puntero,char msg[]);
+void setPoPularidadMensaje(Mensajes* puntero,int popular);
+void idUsuarioRelaa(Mensajes* puntero,int usuarioRela);
+void setidUsuario(Usuarios *puntero,int id);
+void setnick(Usuarios* puntero,char msg[]);
+void intPoPuLaridadUsuario (Usuarios *puntero, int dato);
+void BajaDeCliente(ArrayList* lista);
+void ModificarCliente(ArrayList *lista);
+void AltaCliente(ArrayList* lista,int *puntero);
+void CargarDatosAlSistema(ArrayList* lista1);
+int employee_compareNumero(void* pEmployeeA,void* pEmployeeB);
+void idUsuarioRelaa(Mensajes* puntero,int usuarioRela);
+void setPoPularidadMensaje(Mensajes* puntero,int popular);
+Todo* employee_newPersonaFull(void);
+Usuarios* employee_newUsuario(void);
+Mensajes* employee_newMensaje();
+void employee_printUsuarios(Usuarios *this);
+void CargarUsuariosAlSistema(ArrayList* lista1);
+
+
+
+#endif // _EMPLOYEE_H
--- main.c
+++ main.c
@@ -1,217 +1,87 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-
-
-
-#define C 3
-
-
-
-void mostrarClientes(int[], char[][30], int[], int[], int);
-
+#include "funciones.h"
+#include "ArrayList.h"
+/****************************************************
+    Menu:
+        1. Parse del archivo data.csv
+        2. Listar Empleados
+        3. Ordenar por nombre
+        4. Agregar un elemento
+        5. Elimina un elemento
+        6. Listar Empleados (Desde/Hasta)
+*****************************************************/
+int menu();
 
 int main()
 {
-    //legajo, nombre  edad
 
+    ArrayList* lista = al_newArrayList();
+    ArrayList* lista1 = al_newArrayList();
 
-    int legajos[C]= {6,5,1}, edades[C]= {5,6,7}, estados[C]= {1,1,1}, flag;
-    char nombres[C][30]= {"Maria", "Juan", "Maria"};
-
-
-    char seguir='s', respuesta, auxString[100];
-    int opcion;
-    int i, j;
-    int auxInt;
+    if(lista==NULL)
+    {
+        printf("Error algun puntero es nulo");
+        exit(-1);
+    }
+    char seguir = 's';
 
     do
     {
-        printf("1.Alta\n2. Bajas\n3. Modificaciones\n4. Ordenar\n5. Mostrar\n6. Salir\nElija una opcion: ");
-        scanf("%d", &opcion);
-        switch(opcion)
+        switch(menu())
         {
+
         case 1:
-            flag=0;
-            for(i=0; i<C; i++)
-            {
-                if(estados[i]==0)
-                {
-                    printf("Ingrese legajo: ");
-                    scanf("%d", &legajos[i]);
-                    printf("Ingrese edad: ");
-                    scanf("%d", &edades[i]);
-                    printf("Ingrese nombre: ");
-                    fflush(stdin);
-                    gets(nombres[i]);
-
-                    estados[i] = 1;
-                    flag=1;
-                    break;
-
-                }
-            }
-
-            if(flag==0)
-            {
-                printf("No hay espacio!");
-            }
+            CargarDatosAlSistema(lista);
+            ListarClientes(lista);
+            system("pause");
             break;
-
         case 2:
-            flag =0;
-            printf("Ingrese legajo a eliminar: ");
-            scanf("%d", &auxInt);
-
-            for(i=0; i<C; i++)
-            {
-                if(estados[i]==1 && auxInt == legajos[i])
-                {
-                    printf("%d--%s--%d\n", legajos[i], nombres[i], edades[i]);
-
-                    printf("Esta seguro de eliminar el cliente? s/n: ");
-                    respuesta = getche();
-                    if(respuesta=='s')
-                    {
-                        estados[i] = 0;
-                    }
-                    else
-                    {
-                        printf("Accion cancelada!");
-                    }
-                    flag=1;
-                    break;
-
-                }
-            }
-
-
-
-            if(flag==0)
-            {
-                printf("legajo inexistente");
-            }
-
-
+            CargarUsuariosAlSistema(lista1);
+            MostrarUsuarios(lista1);
+            system("pause");
             break;
-
         case 3:
-            flag =0;
-            printf("Ingrese legajo a modificar: ");
-            scanf("%d", &auxInt);
 
-            for(i=0; i<C; i++)
-            {
-                if(estados[i]==1 && auxInt == legajos[i])
-                {
-                    printf("%d--%s--%d\n", legajos[i], nombres[i], edades[i]);
-                    printf("Ingrese la nueva edad: ");
-                    scanf("%d", &auxInt);
-                    printf("Esta seguro de realizar la modificacion? s/n: ");
-                    respuesta = getche();
-                    if(respuesta=='s')
-                    {
-                        edades[i] = auxInt;
-                    }
-                    else
-                    {
-                        printf("Accion cancelada!");
-                    }
-                    flag=1;
-                    break;
-
-                }
-            }
-
-
-
-            if(flag==0)
-            {
-                printf("legajo inexistente");
-            }
-
-
+            system("pause");
             break;
         case 4:
-
-            for(i=0; i<C-1; i++)
-            {
-                for(j=i+1; j<C; j++)
-                {
-                    if(estados[i]==1 && estados[j]==1)
-                    {
-                        if(strcmp(nombres[i],nombres[j])>0)
-                        {
-                            strcpy(auxString, nombres[i]);
-                            strcpy(nombres[i], nombres[j]);
-                            strcpy(nombres[j],auxString);
-
-                            auxInt = legajos[i];
-                            legajos[i] = legajos[j];
-                            legajos[j] = auxInt;
-
-                            auxInt = edades[i];
-                            edades[i] = edades[j];
-                            edades[j] = auxInt;
-
-                            auxInt = estados[i];
-                            estados[i] = estados[j];
-                            estados[j] = auxInt;
-
-                        }
-                        else
-                        {
-                            if(strcmp(nombres[i],nombres[j])==0)
-                            {
-                                if(legajos[i]>legajos[j])
-                                {
-                                    auxInt = legajos[i];
-                                    legajos[i] = legajos[j];
-                                    legajos[j] = auxInt;
-
-                                    auxInt = edades[i];
-                                    edades[i] = edades[j];
-                                    edades[j] = auxInt;
-
-                                    auxInt = estados[i];
-                                    estados[i] = estados[j];
-                                    estados[j] = auxInt;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-
+            system("pause");
             break;
         case 5:
-            mostrarClientes(legajos, nombres, edades, estados, C);
+            system("pause");
             break;
-
         case 6:
             seguir = 'n';
             break;
         }
-
     }
-    while(seguir=='s');
+    while(seguir == 's');
 
+
+    return 0;
 }
 
-
-void mostrarClientes(int leg[], char nom[][30], int edad[], int est[], int tam)
+int menu()
 {
-    int i;
-    for(i=0; i<C; i++)
-    {
-        if(est[i]!=0)
-        {
-            printf("%d--%s--%d\n", leg[i], nom[i], edad[i]);
-        }
+    int opcion;
+    system("cls");
+    printf("**Menu de Opciones**\n\n");
 
-    }
+    printf("1. LEER ARCHIVO\n");
+    printf("2. COMPLETAR\n");
+    printf("3. ORDENAR LISTA\n");
+    printf("4. MOSTRAR DATOS QUE SE REPITEN\n");
+    printf("5. MOSTRAR DATOS QUE NO SE REPITEN\n");
+    printf("6. SALIR \n");
 
+
+
+    scanf("%d", &opcion);
+
+    return opcion;
 }
+
+
 
